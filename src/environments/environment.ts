f473eb0769c1884910571a1bd95859d03f4726fc
luis pablo <luis.pablo.peralta.tapia@gmail.com>
2024-02-04 13:25:41 -0600

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'auth-login-f9ad6',
+    appId: '1:339911928210:web:e550254a5c0f6455e0b94b',
+    storageBucket: 'auth-login-f9ad6.appspot.com',
+    apiKey: 'AIzaSyA_AZD8gox4OB-QOdsoHCT_v_LfM4i6UPc',
+    authDomain: 'auth-login-f9ad6.firebaseapp.com',
+    messagingSenderId: '339911928210',
+  },
+};
